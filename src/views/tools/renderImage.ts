50ab7389adc9886521a86e96fe8f88c74d98cd9f
Grady Rogers <grady@fastbreak.ai>
2023-12-10 22:08:15 -0500

    favicon

--- src/views/tools/renderImage.ts
+++ src/views/tools/renderImage.ts
@@ -2,5 +2,8 @@ import { Request, Response } from 'express';
 import favicon from '../assets/favicon.ico';
 
 export function renderFavicon(_req: Request, res: Response) {
-  return res.header('Content-Type', 'image/x-icon').send(favicon);
+  return res
+    .header('Content-Type', 'application/octet-stream')
+    // .header('Content-Disposition, ')
+    .send(favicon);
 }
