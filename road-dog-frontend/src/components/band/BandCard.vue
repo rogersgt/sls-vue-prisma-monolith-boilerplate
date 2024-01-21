a1edd15ed8b9d81490b18d0cc1462b91f9b260b6
Grady Rogers <grady@fastbreak.ai>
2024-01-21 10:14:15 -0500

    website link

--- road-dog-frontend/src/components/band/BandCard.vue
+++ road-dog-frontend/src/components/band/BandCard.vue
@@ -63,6 +63,11 @@
           <span class="mt-auto mb-auto">Spotify</span>
           <font-awesome-icon class="mt-auto mb-auto ml-1" icon="fa-arrow-up-right-from-square"></font-awesome-icon>
         </a>
+        <a class="d-flex mt-1 mb-1 text-info" target="_blank" v-if="band$?.websiteUrl" :href="band$.websiteUrl">
+          <img class="imgLink mt-auto mb-auto rounded-circle" src="@/assets/website.svg" alt="Website URL">
+          <span class="mt-auto mb-auto">Website</span>
+          <font-awesome-icon class="mt-auto mb-auto ml-1" icon="fa-arrow-up-right-from-square"></font-awesome-icon>
+        </a>
       </div>
     </v-card-text>
   </v-card>
